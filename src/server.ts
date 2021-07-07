5226bf8b686737f869d0c4ae0eb95c45d6573150
Ricky Kawagishi <ricky.kawagishi@softwire.com>
2021-07-07 11:40:52 +0100

    Can specify camera type for the rover

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,24 @@
 import express from "express";
 import axios from 'axios';
 
+enum RoverName {
+    curiosity = 'curiosity',
+    opportunity = 'opportunity',
+    spirit = 'spirit'
+}
+
+enum RoverCameraType {
+    FHAZ,
+    RHAZ,
+    MAST,
+    CHEMCAM,
+    MAHLI,
+    MARDI,
+    NAVCAM,
+    PANCAM,
+    MINITES
+}
+
 const app = express();
 const port = 8000;
 
@@ -8,17 +26,42 @@ app.use(express.json());
 const router = express.Router();
 router.get('/test', (req, res) => res.send('Hello world !'));
 router.get('/rovers/:roverName', async (req, res) => {
-    // const roverNames =  ['curiosity', 'opportunity', 'spirit']
     const roverName: string = req.params.roverName
-    const apiKey = 'aRWUnNqpH11eSQvCm114SVuAnhMFivU6KLWFgF7k'
-    const sol = 1000
-    const url = `https://api.nasa.gov/mars-photos/api/v1/rovers/${roverName}/photos?sol=${sol}&api_key=${apiKey}`
-    const response = await axios.get(url)
-    const data = response.data
+    const data = await roverPhotoData(roverName, '')
     res.send(data)
 })
+
+router.get('/rovers/:roverName/photos', async (req, res) => {
+    const roverName: string = req.params.roverName
+    const data = await roverPhotoData(roverName, '')
+    res.send(data)
+})
+
+router.get('/rovers/:roverName/photos/:cameraType', async (req, res) => {
+    const roverName: string = req.params.roverName
+    const cameraType: string = req.params.cameraType
+    const data = await roverPhotoData(roverName, cameraType)
+    res.send(data)
+})
+
 app.use('/', router);
 
 app.listen(port, () => {
     console.log(`Test backend is running on port ${port}`);
 });
+
+async function roverPhotoData(roverName: string, cameraType: string) {
+    if (!(roverName in RoverName)) {
+        return 'Check the rover name!'
+    }
+    const cameraTypeUppercase = cameraType.toUpperCase()
+    if (cameraTypeUppercase !== '' && !(cameraTypeUppercase in RoverCameraType)) {
+        return 'Check the rover camera type!'
+    }
+    let cameraTypeQuery = cameraTypeUppercase === '' ? '' : `camera=${cameraTypeUppercase}`
+    const apiKey = 'aRWUnNqpH11eSQvCm114SVuAnhMFivU6KLWFgF7k'
+    const sol = 1000
+    const url = `https://api.nasa.gov/mars-photos/api/v1/rovers/${roverName}/photos?sol=${sol}&${cameraTypeQuery}&api_key=${apiKey}`
+    const response = await axios.get(url)
+    return response.data
+}
