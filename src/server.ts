126e730de2a07dac45157800ad2f9bfd8abad34c
Ricky Kawagishi <ricky.kawagishi@softwire.com>
2021-07-07 11:15:47 +0100

    small change - made sol a variable

--- src/server.ts
+++ src/server.ts
@@ -11,7 +11,8 @@ router.get('/rovers/:roverName', async (req, res) => {
     // const roverNames =  ['curiosity', 'opportunity', 'spirit']
     const roverName: string = req.params.roverName
     const apiKey = 'aRWUnNqpH11eSQvCm114SVuAnhMFivU6KLWFgF7k'
-    const url = `https://api.nasa.gov/mars-photos/api/v1/rovers/${roverName}/photos?sol=1000&api_key=${apiKey}`
+    const sol = 1000
+    const url = `https://api.nasa.gov/mars-photos/api/v1/rovers/${roverName}/photos?sol=${sol}&api_key=${apiKey}`
     const response = await axios.get(url)
     const data = response.data
     res.send(data)
